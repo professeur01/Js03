f9d15accf05006d89ed49410c23678687f90d3c3
professeur01 <124563602+professeur01@users.noreply.github.com>
2023-02-15 22:00:03 +0100

    Update displayBookInfo.js

--- displayBookInfo.js
+++ displayBookInfo.js
@@ -1,12 +1,12 @@
 let book = {
     "Titre" : "L'Enfant Noir ",
-    "Auteur" : "Camara Laye",
-    "Année " : 1953
+    "auteur" : "Camara Laye",
+    "année " : 1953
   }
   
   function displayBookInfo(){
     console.log("Titre:",book.Titre);
     console.log("Auteur:",book.Auteur);
-    console.log("Année :",book["Année "]);
+    console.log("année :",book["année "]);
   }
   displayBookInfo();
